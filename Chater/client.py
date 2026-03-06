97026abe980f826f8026d6b40d3137a4a05bb28c
Aditya Raj <desputhegreat@gmail.com>
2026-03-06 22:36:03 +0530

    Auth

--- Chater/client.py
+++ Chater/client.py
@@ -1,20 +1,53 @@
 #client
 import socket
 import threading
+import queue
 
+user = input("Please enter your username: ")
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 sock.connect(("127.0.0.1", 5000))
+
+msgs = queue.Queue()
+def auth():
+    while True:    
+        sock.send(user.encode())
+        responce = sock.recv(1024).decode()
+        if responce == "Login":
+            password = input("Enter your password: ")
+            sock.send(password.encode())
+            responce = sock.recv(1024).decode()
+            if responce == "Logged in":
+                print("Successfully Logged in!")
+                break
+            elif responce == "Incorrect":
+                password = input("Incorrect Password. Try again: ")
+                sock.send(password.encode())
+            else:
+                print("Something went wrong")
+                break    
+        elif responce == "Register":
+            password = input("Enter a strong password: ")
+            sock.send(password.encode())
+            print("Successfully Registered!")
+            break
+        else:
+            print("Something went wrong")     
+            break
 
 def msg_sender():
     while True:
+        if not msgs.empty():
+            print(msgs.get())
+            msgs.task_done()
         message = input("> ")
         sock.send(message.encode())
+
 def msg_reciever():
     while True:
         msg = sock.recv(1024)
-        print(msg.decode())
-
+        msgs.put(msg.decode())
+auth()
 msg_sender_thread = threading.Thread(target=msg_sender)
 msg_reciever_thread = threading.Thread(target=msg_reciever)
 msg_sender_thread.start()
+msg_reciever_thread.start()
--- Chater/server.py
+++ Chater/server.py
@@ -1,6 +1,8 @@
 #server
 import socket
 import threading
+import json
+import hashlib
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 adr = ("127.0.0.1", 5000)
@@ -10,11 +12,40 @@ print("Waiting for connections...")
 server.listen()
 clients = []
 
+json_dir = "C:/VsCode/Utils/Chater/data/users.json"
+
+with open(json_dir, "r") as file:
+    users = json.load(file)
+
+def auth(client_socket):
+    while True:    
+        user = client_socket.recv(1024).decode()
+        if user in users:
+            msg = "Login"
+            client_socket.send(msg.encode())
+            password = client_socket.recv(1024)
+            if hashlib.sha256(password).hexdigest() == users[user]:
+                msg = "Logged in"
+                client_socket.send(msg.encode())
+                break
+            else:
+                msg = "Incorrect"
+                client_socket.send(msg.encode())
+                password = client_socket.recv(1024)
+        else:
+            msg = "Register"
+            client_socket.send(msg.encode()) 
+            password = client_socket.recv(1024)
+            users[user] = hashlib.sha256(password).hexdigest()
+            with open(json_dir, "w") as file:
+                json.dump(users, file, indent=4)
+            break
+
 def accepter():
     while True:    
-        client_socket, client_addr = server.accept()
+        client_socket, _ = server.accept()
         clients.append(client_socket)
-        print(f"{client_addr} joined")
+        auth(client_socket)
         client_thread = threading.Thread(target=recv_brod, args=(client_socket,)) 
         client_thread.start()
 
@@ -35,9 +66,8 @@ def broadcast(msg, sender):
     for client_socket in clients:
         if client_socket != sender:
             client_socket.send(msg)
-            
-accepter_thread = threading.Thread(target=accepter)
 
+accepter_thread = threading.Thread(target=accepter)
 accepter_thread.start()
 
     
